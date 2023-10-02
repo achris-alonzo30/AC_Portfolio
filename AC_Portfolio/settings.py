b0b78a5de47d7405e31ea9b8afe5b33f96c80836
achris-alonzo30 <achris.alonzo30@gmail.com>
2023-10-02 12:05:12 -0300

    Update settings.py

--- AC_Portfolio/settings.py
+++ AC_Portfolio/settings.py
@@ -15,7 +15,7 @@ import os
 from decouple import config
 
 # vercel_app/settings.py
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
