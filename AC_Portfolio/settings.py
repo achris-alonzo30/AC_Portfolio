51187d012ed210ee551265da944a0692255a59cf
achris-alonzo30 <achris.alonzo30@gmail.com>
2023-10-02 13:23:26 -0300

    Update settings.py and .env file

--- AC_Portfolio/settings.py
+++ AC_Portfolio/settings.py
@@ -15,18 +15,11 @@ import os
 from decouple import config
 
 # vercel_app/settings.py
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DJANGO_SECRET_KEY = config('DJANGO_SECRET_KEY')
-ENGINE = 'django.db.backends.postgresql'
-URL = config('URL')
-NAME = config('NAME')
-USER = config('USER')
-PASSWORD = config('PASSWORD')
-HOST = config('HOST')
-PORT = config('PORT')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -83,17 +76,7 @@ WSGI_APPLICATION = 'AC_Portfolio.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'URL': URL,
-        'NAME': NAME,
-        'USER': USER,
-        'PASSWORD': PASSWORD,
-        'HOST': HOST,
-        'PORT': PORT,
-    }
-}
+DATABASES = {}
 
 
 # Password validation
