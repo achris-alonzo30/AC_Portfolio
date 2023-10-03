014db96730cca19b09f0fcbbbbb125b1cb9d38ae
achris-alonzo30 <achris.alonzo30@gmail.com>
2023-10-03 12:14:25 -0300

    Update header tag

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("project-list", views.project_list, name="project-list"),
     path("resume", views.view_resume, name="resume"),
+
 ]
